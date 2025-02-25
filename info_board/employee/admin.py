2c23f52363c36bace2d5a9244b41179f5f313886
alex004 <alexander.kintsler@yandex.ru>
2025-02-25 10:09:13 +0300

    edit admin settings

--- info_board/employee/admin.py
+++ info_board/employee/admin.py
@@ -4,9 +4,9 @@ from info_board.employee.models import Contact, Employee
 
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'patronymic', 'last_name', 'updated_at']
+    list_display = ['id', 'first_name', 'patronymic', 'last_name', 'updated_at']
 
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ['employee', 'contact_type']
+    list_display = ['id', 'employee', 'contact_type']
--- info_board/schedule/admin.py
+++ info_board/schedule/admin.py
@@ -3,14 +3,14 @@ from info_board.schedule.models import Faculty, StudentsGroup, ScheduleEntry
 
 @admin.register(Faculty)
 class FacultyAdmin(admin.ModelAdmin):
-    list_display = ['short_name']
+    list_display = ['id', 'short_name']
 
 
 @admin.register(StudentsGroup)
 class StudentsGroupAdmin(admin.ModelAdmin):
-    list_display = ['name', 'course_number', 'faculty', 'updated_at']
+    list_display = ['id', 'name', 'course_number', 'faculty', 'updated_at']
 
 
 @admin.register(ScheduleEntry)
 class ScheduleEntryAdmin(admin.ModelAdmin):
-    list_display = ['students_group', 'day_of_week', 'type_of_week']
+    list_display = ['id', 'students_group', 'day_of_week', 'type_of_week', 'entry']
