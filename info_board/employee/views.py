d16391c0c409c47e305ed9ab5c799e11174d6f40
alex004 <alexander.kintsler@yandex.ru>
2025-03-04 23:05:34 +0300

    static, media

--- info_board/employee/views.py
+++ info_board/employee/views.py
@@ -1,9 +1,21 @@
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework.generics import ListAPIView
 
 from info_board.employee.models import Employee
 from info_board.employee.serializers import EmployeeContactSerializer
 
-
+@extend_schema(
+    parameters=[
+        OpenApiParameter(
+            name='query',
+            type=OpenApiTypes.STR,
+            description='Запрос для поиска преподавателей',
+            location=OpenApiParameter.QUERY,
+            required=False
+        )
+    ]
+)
 class EmployeeListView(ListAPIView):
     serializer_class = EmployeeContactSerializer
 
